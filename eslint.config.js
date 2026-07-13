160abfa8fcc2bfa9915221692bfd99bd69c226ce
Shah Hussain <95882307+shhahhussain@users.noreply.github.com>
2026-07-13 16:21:00 +0500

    chore: add soft eslint config

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import tseslint from 'typescript-eslint'
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'warn',
+    },
+    rules: {
+      'no-var': 'warn',
+      'prefer-const': 'warn',
+      'no-debugger': 'warn',
+      'eqeqeq': ['warn', 'always'],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+]
